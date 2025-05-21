0d5fb5ed29fa8ddc51d30531e070f1f10d6ef770
Artem <artemalesin89778@gmail.com>
2025-05-21 15:06:45 +0300

    fix changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import ProductPage from './pages/ProductPage';
 import NotFound from './pages/NotFound';
